bdf32f92c4300701446488d20c0d1576614d1898
rudrkrishna <Kittu2909@>
2023-02-23 08:45:35 +0530

    controller added

--- src/main/java/com/CICD/controller/TestController.java
+++ src/main/java/com/CICD/controller/TestController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-
     @GetMapping("/checkStatus")
     public ResponseEntity<String> checkStatus(){
         return new ResponseEntity<>("Service is up and running", HttpStatus.OK);
