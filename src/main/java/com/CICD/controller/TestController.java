2ae2616359af727e6f9b7f5ab5570808e6ad6bfd
rudrkrishna <Kittu2909@>
2023-02-23 08:42:32 +0530

    controller added

--- src/main/java/com/CICD/controller/TestController.java
+++ src/main/java/com/CICD/controller/TestController.java
@@ -1,2 +1,15 @@
-package com.CICD.controller;public class TestController {
+package com.CICD.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/checkStatus")
+    public ResponseEntity<String> checkStatus(){
+        return new ResponseEntity<>("Service is up and running", HttpStatus.OK);
+    }
 }
